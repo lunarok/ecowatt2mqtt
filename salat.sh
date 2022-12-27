24c54d668ed93c4e189029d3a9618e889dad2eb6
Cédric Dupont <lunarok@gmail.com>
2022-12-27 14:47:15 +0100

    Update salat.sh

--- salat.sh
+++ salat.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
 
-if [[ -z "${MQTT_HOST}" ]]; then
-  mqtt_host="192.168.0.100"
-else
-  mqtt_host=${MQTT_HOST}
-fi
+mqtt_host="${MQTT_HOST:-192.168.0.100}"
+mqtt_topic="${MQTT_TOPIC:-salat/time}"
+latitude="${LATITUDE:-47,47}"
+longitude="${LONGITUDE:--0,64}"
 
-if [[ -z "${MQTT_TOPIC}" ]]; then
-  mqtt_topic="salat/time"
-else
-  mqtt_topic=${MQTT_TOPIC}
-fi
-
-if [[ -z "${LATITUDE}" ]]; then
-  latitude="47,47"
-else
-  latitude=${LATITUDE}
-fi
-
-if [[ -z "${LONGITUDE}" ]]; then
-  longitude="-0,64"
-else
-  longitude=${LONGITUDE}
-fi
-
-array=( (ipraytime --latitude $latitude --longitude $longitude -a 1 --fajrangle 12 --ishaangle 12) )
+array=( $(ipraytime --latitude ${latitude} --longitude ${longitude} -a 1 --fajrangle 12 --ishaangle 12) )
 json='{"imsak":"'`date --date ${array[56]}`'","fajr":"'`date --date ${array[47]}`'","shurooq":"'`date --date ${array[48]}`'","dhuhr":"'`date --date ${array[49]}`'","asr":"'`date --date ${array[50]}`'","maghrib":"'`date --date ${array[51]}`'","isha":"'`date --date ${array[52]}`'","imsak_tomorrow":"'`date --date ${array[60]}`'","fajr_tomorrow":"'`date --date ${array[64]}`'"}'
 
 mosquitto_pub -r -h $mqtt_host -t homeassistant/sensor/salat/imsak/config -m '{"name": "Imsak", "device_class": "date", "state_topic": "'${mqtt_topic}'", "value_template": "{{ value_json.imsak}}", "unique_id": "salat.imsak"}
